84bf21eafaee5416571cffdd75bb887ca08fe86b
ajaz shajahan <ajazshajahan@gmail.com>
2024-10-26 09:19:39 +0530

    readify api first commit

---
+++ src/main/java/com/readify/readify/ReadifyApplication.java
@@ -0,0 +1,13 @@
+package com.readify.readify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readify/readify/ReadifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readify.readify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
